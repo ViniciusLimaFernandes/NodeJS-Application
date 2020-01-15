04968246051ab0be5ff0b64dad720052e3ded6ae
Vinicius Lima Fernandes <vfernandes@uolinc.com>
2020-01-15 14:52:01 -0300

    Aprendendo promises

--- index.js
+++ index.js
@@ -1,56 +1,67 @@
-function obterUsuario(callback) {
-  setTimeout(() => {
-    return callback(null, {
-      id: 1,
-      nome: "Aladin",
-      dataNascimento: new Date()
-    });
-  }, 1000);
-}
-
-function obterTelefone(idUsuario, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      telefone: "11946842684",
-      ddd: 11
-    });
-  }, 2000);
-}
-
-function obterEndereco(idUsuario, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      rua: "Avenida pinheiros",
-      numero: 1358
-    });
-  }, 2000);
-}
-
-function resolverUsuario(erro, usuario) {
-  console.log("usuario", usuario);
-}
-
-obterUsuario(function resolverUsuario(error, usuario) {
-  if (error) {
-    console.error("User error", error);
-    return;
-  }
-  obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
-    if (error1) {
-      console.error("Tel error", error1);
-      return;
-    }
-    obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
-      if (error2) {
-        console.error("End error", error2);
-        return;
-      }
-
-      console.log(`
-        Nome: ${usuario.nome},
-        Endereco: ${endereco.rua}, ${endereco.numero}
-        Telefone: ${telefone.telefone}
-      `);
-    });
+function obterUsuario() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve({
+        id: 1,
+        nome: "Aladin",
+        dataNascimento: new Date()
+      });
+    }, 1000);
   });
-});
+}
+
+function obterTelefone(idUsuario) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve({
+        telefone: "11946842684",
+        ddd: 11
+      });
+    }, 2000);
+  });
+}
+
+function obterEndereco(idUsuario) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve({
+        rua: "Avenida pinheiros",
+        numero: 1358
+      });
+    }, 2000);
+  });
+}
+
+const usuarioPromise = obterUsuario();
+
+usuarioPromise
+  .then(usuario => {
+    return obterTelefone(usuario.id).then(telefone => {
+      return {
+        usuario: {
+          nome: usuario.nome,
+          id: usuario.id
+        },
+        telefone: telefone
+      };
+    });
+  })
+  .then(result => {
+    return obterEndereco(result.usuario.id).then(endereco => {
+      return {
+        usuario: result.usuario,
+        telefone: result.telefone,
+        endereco: endereco
+      };
+    });
+  })
+  .then(result => {
+    console.log(`
+        Nome: ${result.usuario.nome}
+        Telefone: ${result.telefone.telefone}
+        Endereco: ${result.endereco.rua}, ${result.endereco.numero}
+    `);
+  })
+  .catch(error => {
+    console.error("Error while processing the user...", error);
+  });
